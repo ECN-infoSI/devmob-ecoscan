81aa72f2d46ba573b89b319f9fcfe0580ab680c0
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2025-03-21 11:15:42 +0100

    Details du produit

--- app/src/main/java/com/example/ecoscan/MainActivity.kt
+++ app/src/main/java/com/example/ecoscan/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.ecoscan
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ecoscan.ui.theme.EcoScanTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EcoScanTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Content(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +32,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Content(modifier: Modifier = Modifier) {
+    // Obtém o contexto atual
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Hello Android!")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            // Inicia a ProductDetailActivity
+            context.startActivity(Intent(context, ProductDetailActivity::class.java))
+        }) {
+            Text("Abrir Detalhes do Produto")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EcoScanTheme {
-        Greeting("Android")
+        Content()
     }
 }
