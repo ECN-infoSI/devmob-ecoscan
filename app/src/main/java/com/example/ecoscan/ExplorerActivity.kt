d7876470db5d605b5bfc123c79e4500270d4a237
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-03-25 17:51:23 +0100

    Try to fix bugs on the beggining of the app

--- app/src/main/java/com/example/ecoscan/ExplorerActivity.kt
+++ app/src/main/java/com/example/ecoscan/ExplorerActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ExplorerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +37,7 @@ class ExplorerActivity : AppCompatActivity() {
         findViewById<RecyclerView>(R.id.recyclerViewLabels).adapter = LabelAdapter(getLabels())
 
         // Configurar botão scanner
-        findViewById<Button>(R.id.fab_scanner).setOnClickListener {
+        findViewById<FloatingActionButton>(R.id.fab_scanner).setOnClickListener {
             Toast.makeText(this, "Scanner ouvert!", Toast.LENGTH_SHORT).show()
         }
 
