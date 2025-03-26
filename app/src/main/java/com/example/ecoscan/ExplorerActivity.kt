f3e2bdbe4e54b69b715a4dc5208576dd1e8da42c
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-03-26 17:14:06 +0100

    Add product description and fix scroll view

--- app/src/main/java/com/example/ecoscan/ExplorerActivity.kt
+++ app/src/main/java/com/example/ecoscan/ExplorerActivity.kt
@@ -2,27 +2,13 @@ package com.example.ecoscan
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.ListView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.appcompat.widget.SearchView
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.SearchView
 import com.example.ecoscan.databinding.ActivityExplorerBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-
 
 class ExplorerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExplorerBinding
-    //private var lateinit searchView: SearchView;
-    //private var lateinit resultsView: ListView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,10 +27,9 @@ class ExplorerActivity : AppCompatActivity() {
             }
         }
 
-
-        //Gestion du moteur de recherche
-        //searchView = findViewById(R.id.searchView);
-        //resultsView = findViewById(R.id.resultsView);
+        //Definition de la phrase dans la barre de recherche
+        val searchView = findViewById<SearchView>(R.id.searchView)
+        searchView.queryHint = getString(R.string.rechercher)
 
         //Gestion des clicks sur des produits
         binding.ivrProductLait.setOnClickListener {
