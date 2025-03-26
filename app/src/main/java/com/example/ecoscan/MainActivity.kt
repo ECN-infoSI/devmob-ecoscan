279f59e63166d36a25c4cb9390e319d2be443a3a
Clara Mattos <mattosclara22@gmail.com>
2025-03-26 22:27:19 +0100

    test

--- app/src/main/java/com/example/ecoscan/MainActivity.kt
+++ app/src/main/java/com/example/ecoscan/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ecoscan
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,55 +16,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.ecoscan.databinding.ActivityLabelDetailBinding
 import com.example.ecoscan.ui.theme.EcoScanTheme
+import com.example.ecoscan.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EcoScanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Content(Modifier.padding(innerPadding))
-                }
-            }
+        // Inicializa o ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Ação do botão de escanear (simulado)
+        binding.scanButton.setOnClickListener {
+            Toast.makeText(this, "Produit scanné, ouverture des détails", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ProductDetailActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Ação do botão de explorar
+        binding.exploreButton.setOnClickListener {
+            val intent = Intent(this, ExplorerActivity::class.java)
+            startActivity(intent)
         }
     }
 }
 
-@Composable
-fun Content(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Hello Android!")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            // Démarre l'activité ProductDetailActivity
-            context.startActivity(Intent(context, ProductDetailActivity::class.java))
-        }) {
-            Text("Montrer les details du produit")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {
-
-            // Démarre l'activité ExplorerActivity
-            context.startActivity(Intent(context, ExplorerActivity::class.java))
-        }) {
-            Text("Montrer la page initial")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EcoScanTheme {
-        Content()
-    }
-}
