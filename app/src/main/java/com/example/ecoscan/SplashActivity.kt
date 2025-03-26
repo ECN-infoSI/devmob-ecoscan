c05dc0fb8e87371ab22123aa7e104acbb2e0e133
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2025-03-26 20:56:23 +0100

    Corrections des libraries

--- app/src/main/java/com/example/ecoscan/SplashActivity.kt
+++ app/src/main/java/com/example/ecoscan/SplashActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import com.example.ecoscan.ui.theme.EcoScanTheme
 
