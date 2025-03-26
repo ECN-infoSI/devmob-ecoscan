d1c007d51e80b9bd71eb3aea709dbef269a58e9e
Larissa Albuquerque <larissaanogueira24@gmail.com>
2025-03-26 08:13:03 +0100

    Integrate all pages

--- app/src/main/java/com/example/ecoscan/SplashActivity.kt
+++ app/src/main/java/com/example/ecoscan/SplashActivity.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.ecoscan.ui.theme.EcoScanTheme
 import kotlinx.coroutines.delay
+import com.example.ecoscan.ui.theme.EcoScanTheme
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
